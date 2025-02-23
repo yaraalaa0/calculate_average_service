910dd178ed2bf0f5ee03ffb2e6601e2bad0addba
yaraalaa0 <37335420+yaraalaa0@users.noreply.github.com>
2025-02-23 20:37:33 +0200

    Update average_server.cpp

--- average_server.cpp
+++ average_server.cpp
@@ -1,6 +1,9 @@
 #include "ros/ros.h"
 #include "my_srv/Average.h"
 
+/**
+* The service function that calculates the average between two numbers
+*/
 bool averageTwoNum(my_srv::Average::Request &req, my_srv::Average::Response &res){
     res.result= (req.num1+req.num2)/2.0;
     return true;
@@ -13,7 +16,9 @@ int main(int argc, char **argv)
    */
   ros::init(argc, argv, "average_server");
   ros::NodeHandle n;
-  
+  /**
+   * Define the ROS server
+   */
   ros::ServiceServer a_server = n.advertiseService("/average", averageTwoNum);
   ros::spin();
   return 0;
